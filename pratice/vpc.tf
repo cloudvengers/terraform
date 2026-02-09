52fa943368dd5c0cda483829e9ddf7b787ed3984
root <root@Ki.localdomain>
2026-02-10 02:08:34 +0900

    3-tier

---
+++ pratice/vpc.tf
@@ -0,0 +1,68 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main_vpc"
+  }
+}
+
+resource "aws_subnet" "public_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "public-subnet-a"
+  }
+}
+
+resource "aws_subnet" "public_c" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "public-subnet-c"
+  }
+}
+
+resource "aws_subnet" "private_app_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.11.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private-app-subnet-a"
+  }
+}
+
+resource "aws_subnet" "private_app_c" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.12.0/24"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "private-app-subnet-c"
+  }
+}
+
+resource "aws_subnet" "private_db_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.21.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private-db-subnet-a"
+  }
+}
+
+resource "aws_subnet" "private_db_c" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.22.0/24"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "private-db-subnet-c"
+  }
+}
